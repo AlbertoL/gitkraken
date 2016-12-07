901e0024707ed6507088ece96f55d577ac6eab80
Alberto López <f1k3f1k3@gmail.com>
2016-12-06 22:02:14 -0300

    embarra

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="estilos.css">
-</head>
-<body>
-<h1>Hemos hecho otro cambio</h1>
-	<p>Este es un parrafo editado en github web</p>
-	<p>este es el segundo párrafo</p>	
-</body>
-</html>
+index.php
